fa3e7cd97c8724a891bf6264471ded40ca84ce39
Lokesh <lokesh.chaudhari.st@gmail.com>
2026-09-03 23:16:28 +0530

    Add Product_Of_Array_Except_Self solution

---
+++ Stack/Product_Of_Array_Except_Self.java
@@ -0,0 +1,38 @@
+/*  
+    Problem : Porduct Of Array Except Self   
+    
+    Idea:
+    Use a prefix / postfix or left or right concept.
+    We multiple indexes left/ prefix of Index i ;
+    and the same with indexes on right / postfix of INdex i.
+    and then we multiple both.
+
+    Time: O(n)
+    Space: O(1)
+
+    The Solution here is Simply a logic 
+    and will not run without Main() method.
+
+*/ 
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[n];
+
+        int product = 1;
+        // for left elements
+        for (int i = 0 ; i < n ; i++){
+            ans[i] = product;
+            product *= nums[i];
+        }
+
+        product = 1;
+        // for right elements
+        for (int i = n - 1 ; i >= 0 ; i--){
+            ans[i] *= product;
+            product *= nums[i];
+        }
+        return ans;
+    }
+}
